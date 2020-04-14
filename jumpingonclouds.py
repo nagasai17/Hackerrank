9a542398c96d28142c400ef1fd887f1957c2a55f
Nagasai Shanmuka Sreenivas <62430771+nagasai17@users.noreply.github.com>
2020-04-14 14:58:23 +0530

    Create jumpingonclouds.py

---
+++ jumpingonclouds.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c):
+…    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
