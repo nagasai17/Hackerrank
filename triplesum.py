5c5eb086322d3213cec61cb0b775ec27d1230f6f
Nagasai Shanmuka Sreenivas <62430771+nagasai17@users.noreply.github.com>
2020-04-15 11:59:21 +0530

    Create triplesum.py

---
+++ triplesum.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the triplets function below.
+def triplets(a, b, c):
+…    fptr.write(str(ans) + '\n')
+
+    fptr.close()
