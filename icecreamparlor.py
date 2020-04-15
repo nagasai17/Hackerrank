0ab29e889028e3c400774bfab0e308aece1c3197
Nagasai Shanmuka Sreenivas <62430771+nagasai17@users.noreply.github.com>
2020-04-15 11:11:51 +0530

    Create icecreamparlor.py

---
+++ icecreamparlor.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the whatFlavors function below.
+def whatFlavors( money,cost):
+…        whatFlavors(cost, money)
